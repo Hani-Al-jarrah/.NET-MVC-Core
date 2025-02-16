f603dfdbe540979e5905ce03f7d54d295449bec7
Hani-2002 <hanijarrah20@gmail.com>
2025-02-17 00:23:37 +0300

    h

---
+++ Task1/Controllers/AirbnbsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task1.Controllers
+{
+    public class AirbnbsController : Controller
+    {
+        public IActionResult Airbnbs()
+        {
+            return View();
+        } 
+        public IActionResult Detailes()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task1/Controllers/ToursController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task1.Controllers
+{
+    public class ToursController : Controller
+    {
+        public IActionResult Tours()
+        {
+            return View();
+        }
+
+        public IActionResult Detailes()
+        {
+            return View();
+        }
+
+    }
+}
